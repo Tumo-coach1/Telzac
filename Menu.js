ffd86d91623329a6eab7e19c742692a36ea20fa5
Johann LACAZE <johann.lacaze@auvergnerhonealpes-orientation.fr>
2023-02-25 10:22:00 +0100

    git commit "1er push"

--- Menu.js
+++ Menu.js
@@ -1,15 +1,2 @@
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let timeRef = document.querySelector('timerdisplay');
-let int = null;
 
-document.getElementById('startTimer')
-	.addEventListener('click', () => {
-		if (int!==null) {
-			clearInterval(int);
-		}
-	if milliseconds = 1000 {
-		int = setTimeout(displayTimer){()
-		milliseconds ++
-		}, 1000);
-	}
-})
+
